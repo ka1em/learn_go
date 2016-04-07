9976db690fe20a818d8ce53d0d5aa46d91213e2c
ka1em <wangkaiminchn@gmail.com>
2016-04-07 16:34:08 +0800

    change line(files), debug

--- src/dup/dup2.go
+++ src/dup/dup2.go
@@ -9,11 +9,11 @@ import(
 func main() {
 	counts := make(map[string]int)
 	files := os.Args[1:]
-	if len(files == 0) {
+	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
-			if err := os.Open(arg)
+			f, err := os.Open(arg)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
@@ -23,7 +23,14 @@ func main() {
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf()
+			fmt.Printf("%d\t%s\n", n, line)
 		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
 	}
 }
