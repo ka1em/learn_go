c58ac565efedc71af06dcef139fd871bba160251
ka1em <wangkaiminchn@gmail.com>
2016-04-18 19:30:41 +0800

    add comment

--- src/basename/basename1.go
+++ src/basename/basename1.go
@@ -1,6 +1,8 @@
 package basename1
 
 // basename ...
+
+// a => a, a.go => a.go. a/b/c.go => c.go
 func basename(s string) string  {
 	for i := len(s) - 1; i >= 0; i-- {
 		if s[i] = '/' {
@@ -8,7 +10,7 @@ func basename(s string) string  {
 			break
 		}
 	}
-
+	// Oreserve everything before last '.'
 	for i := len(s) -1; i >= 0; i-- {
 		if s[i] == '.' {
 			s = s[:i]
