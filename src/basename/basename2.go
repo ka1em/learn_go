bdc34c33e37bd058179fd71247d5fedf380ff917
ka1em <wangkaiminchn@gmail.com>
2016-04-18 19:27:43 +0800

    add basename2.go

---
+++ src/basename/basename2.go
@@ -0,0 +1,13 @@
+package basename2
+
+import "strings"
+
+// basename ...
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/") // -1 if "/" not found
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
