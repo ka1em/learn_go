080e0f84014456c2e809c0b8850e30e88c3c83ec
ka1em <wangkaiminchn@gmail.com>
2016-04-26 22:34:46 +0800

    add reb.go

---
+++ src/rev/rev.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+// rev ...
+func rev(s []int)  {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func main() {
+	a := [...]int{0, 1, 2, 3}
+	rev(a[:])
+	fmt.Println(a)
+}
+
